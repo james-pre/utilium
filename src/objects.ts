f40a391b3ce1163db214d937803c64b638b9691f
James P <jp@drvortex.dev>
2024-04-19 10:43:50 -0500

    Fixed readonly

--- src/objects.ts
+++ src/objects.ts
@@ -5,11 +5,11 @@ export function filterObject<O extends object, R extends object>(object: O, pred
 	return <R>Object.fromEntries(entries.filter(([key, value]) => predicate(key, value)));
 }
 
-export function pick<T extends object, K extends keyof T>(object: T, ...keys: readonly K[] | readonly K[][]): Pick<T, K> {
+export function pick<T extends object, K extends keyof T>(object: T, ...keys: readonly K[] | readonly (readonly K[])[]): Pick<T, K> {
 	return filterObject<T, Pick<T, K>>(object, (key: K) => keys.flat().includes(key));
 }
 
-export function omit<T extends object, K extends keyof T>(object: T, ...keys: readonly K[] | readonly K[][]): Omit<T, K> {
+export function omit<T extends object, K extends keyof T>(object: T, ...keys: readonly K[] | readonly (readonly K[])[]): Omit<T, K> {
 	return filterObject<T, Omit<T, K>>(object, (key: K) => !keys.flat().includes(key));
 }
 
