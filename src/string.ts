ece4051ae3c96b7f2a3317d72e111bc77ee3182c
James Prevett <jp@jamespre.dev>
2024-10-05 23:24:19 -0500

    Formatting

--- src/string.ts
+++ src/string.ts
@@ -1,9 +1,9 @@
 export function capitalize<T extends string>(value: T): Capitalize<T> {
-	return value.at(0)!.toUpperCase() + value.slice(1) as Capitalize<T>;
+	return (value.at(0)!.toUpperCase() + value.slice(1)) as Capitalize<T>;
 }
 
 export function uncapitalize<T extends string>(value: T): Uncapitalize<T> {
-	return value.at(0)!.toLowerCase() + value.slice(1) as Uncapitalize<T>;
+	return (value.at(0)!.toLowerCase() + value.slice(1)) as Uncapitalize<T>;
 }
 
 export type ConcatString<T extends string[]> = T extends [infer F extends string, ...infer R extends string[]] ? `${F}${ConcatString<R>}` : '';
