0741b206af35edb0f5ca3bb2b82b7b8c2f40ad78
James Prevett <jp@jamespre.dev>
2024-10-12 16:57:28 -0500

    Added upload and download

---
+++ src/dom.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
+/**
+ * Upload a file
+ * @todo use Promise.withResolvers
+ */
+export function upload(type: string, multiple = false): Promise<File> {
+	return new Promise<File>((resolve, reject) => {
+		const input = document.createElement('input');
+		input.type = 'file';
+		if (type) input.accept = type;
+
+		if (multiple) input.multiple = true;
+
+		input.addEventListener('change', e => {
+			const file = input.files[0];
+			file ? resolve(file) : reject(new ReferenceError('No files uploaded'));
+		});
+
+		input.click();
+	});
+}
+
+/**
+ * Downloads some data
+ */
+export function download(data: BlobPart, name: string): void {
+	const link = document.createElement('a');
+	link.href = URL.createObjectURL(new Blob([data]));
+	link.download = name;
+	link.click();
+}
