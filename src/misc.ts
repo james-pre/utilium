bb78ca1a684d90a6fa7f52adb18f06034a6b6e70
James Prevett <jp@jamespre.dev>
2025-03-14 14:34:08 -0500

    Added `@memoize`

--- src/misc.ts
+++ src/misc.ts
@@ -52,3 +52,29 @@ export function canary(error: Error = new Error()) {
 export function _throw(e: unknown): never {
 	throw e;
 }
+
+interface MemoizeMetadata extends DecoratorMetadata {
+	memoized?: Record<PropertyKey, any>;
+}
+
+/**
+ * Decorator for memoizing the result of a getter.
+ */
+export function memoize<T, This>(
+	get: () => T,
+	context: ClassGetterDecoratorContext<This, T> & { metadata?: MemoizeMetadata }
+) {
+	if (context.kind != 'getter') throw new Error('@memoize can only be used on getters');
+
+	return function (this: This): T {
+		context.metadata ??= {};
+		const { memoized = {} } = context.metadata;
+
+		if (context.name in memoized) {
+			console.log('Using cached value for', context.name, JSON.stringify(memoized[context.name]));
+			return memoized[context.name];
+		}
+		memoized[context.name] = get.call(this);
+		return memoized[context.name];
+	};
+}
