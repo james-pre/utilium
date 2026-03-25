148ebca603d581051e3cb97ee379dd16aabe98d2
James Prevett <jp@jamespre.dev>
2026-03-25 16:40:25 -0500

    Revert "Fixed `@memoize`"
    This reverts commit cffd5500ee4992000329f2ba996dd2d2bebbb4c3.

--- src/misc.ts
+++ src/misc.ts
@@ -59,20 +59,28 @@ export function _throw(e: unknown): never {
 	throw e;
 }
 
-const missing = Symbol('not memoized');
+interface MemoizeMetadata extends DecoratorMetadata {
+	memoized?: Record<PropertyKey, any>;
+}
 
 /**
  * Decorator for memoizing the result of a getter.
  */
-export function memoize<T, This>(get: () => T, context: ClassGetterDecoratorContext<This, T>) {
+export function memoize<T, This>(
+	get: () => T,
+	context: ClassGetterDecoratorContext<This, T> & { metadata?: MemoizeMetadata }
+) {
 	if (context.kind != 'getter') throw new Error('@memoize can only be used on getters');
 
-	let value: T | typeof missing = missing;
 	return function (this: This): T {
-		if (value !== missing) {
-			return value;
+		context.metadata ??= {};
+		const { memoized = {} } = context.metadata;
+
+		if (context.name in memoized) {
+			console.log('Using cached value for', context.name, JSON.stringify(memoized[context.name]));
+			return memoized[context.name];
 		}
-		value = get.call(this);
-		return value;
+		memoized[context.name] = get.call(this);
+		return memoized[context.name];
 	};
 }
