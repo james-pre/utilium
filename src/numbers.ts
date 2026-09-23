af1350aca35d2249c4a6e730c490013830d5a8af
James Prevett <jp@jamespre.dev>
2026-09-22 19:14:52 -0500

    Added `parseBytes`

--- src/numbers.ts
+++ src/numbers.ts
@@ -26,3 +26,21 @@ export const formatCompact = __formatter.format.bind(__formatter);
 export type ToNumber<T extends CoercibleToString> = `${T}` extends `${infer U extends number}` ? U : never;
 
 export type ToBigInt<T extends CoercibleToString> = `${T}` extends `${infer U extends bigint}` ? U : never;
+
+const byteSizePattern = /^(?<size>\d+(?:\.\d+)?)\s*(?<unit>\w*)$/i;
+const units = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
+
+/**
+ * Parse a string containing some number of bytes, e.g. `1 GiB` or `99.4kb`
+ */
+export function parseBytes(input: string): bigint | null {
+	const match = byteSizePattern.exec(input.trim());
+	if (!match) return null;
+	const size = BigInt(match.groups!.size);
+	let unit = match.groups!.unit.toUpperCase();
+	if (unit.at(-1) == 'B') unit = unit.slice(0, -1);
+	if (unit.at(-1) == 'I') unit = unit.slice(0, -1);
+	const unitIndex = BigInt(units.indexOf(unit));
+	if (unitIndex === -1n) return null;
+	return size * 1024n ** unitIndex;
+}
